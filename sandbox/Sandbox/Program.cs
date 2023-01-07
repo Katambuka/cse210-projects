5d86812b7262856f26a23bb55625b8a1f0295e18
Katambuka <kan22001@byui.edu>
2023-01-07 23:03:01 +0400

    prep2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,12 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("My name is Nathane K.");
+        Console.WriteLine("What's your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"My name is {name}");
     }
 }
