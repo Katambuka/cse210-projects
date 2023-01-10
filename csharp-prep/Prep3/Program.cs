ce7765639c6d3f4f088c42f7bc9237c3a02d30c0
Katambuka <kan22001@byui.edu>
2023-01-10 23:17:24 +0400

    Some idea is coming now

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess Random Number");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 91);
+
+        int guess = -1;
+       
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Your guess number is higher, try again");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("You guessed a lower number, try again");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!. congraculations!!");
+            }
+
+        }                    
     }
 }
