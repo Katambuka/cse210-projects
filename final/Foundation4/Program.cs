813d36183de8b818de56f52b53773e32f15fa2b0
Katambuka <kan22001@byui.edu>
2023-04-02 00:15:51 +0400

    fitness program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        RunningActivity running = new RunningActivity(DateTime.Now, 30, 3.0);
+        activities.Add(running);
+
+        CyclingActivity cycling = new CyclingActivity(DateTime.Now, 45, 15.0);
+        activities.Add(cycling);
+
+        SwimmingActivity swimming = new SwimmingActivity(DateTime.Now, 60, 20);
+        activities.Add(swimming);
+
+        Console.WriteLine("\nMy Fitness Activity Tracker Program:");
+        Console.WriteLine("**************************************");
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+
+        Console.ReadKey();
     }
 }
